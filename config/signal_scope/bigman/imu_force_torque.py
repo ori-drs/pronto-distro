65575f055f824b4ca451fa578bcbfeec0393dc9a
Yukitoshi Minami <yukitoshi.minami@iit.it>
2016-02-12 15:50:38 +0100

    Changes to run latest walkman log

--- config/signal_scope/bigman/imu_force_torque.py
+++ config/signal_scope/bigman/imu_force_torque.py
@@ -9,6 +9,12 @@ def accelNormFunction(msg):
     return msg.utime, numpy.linalg.norm(msg.accel)
 addSignalFunction('MICROSTRAIN_INS', accelNormFunction)
 
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.accel[0])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.accel[1])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.accel[2])
+
+addSignalFunction('MICROSTRAIN_INS2', accelNormFunction)
+
 addSignal('ATLAS_IMU_BATCH',msg.utime, msg.raw_imu[0].linear_acceleration[0])
 addSignal('ATLAS_IMU_BATCH',msg.utime, msg.raw_imu[0].linear_acceleration[1])
 addSignal('ATLAS_IMU_BATCH',msg.utime, msg.raw_imu[0].linear_acceleration[2])
@@ -23,6 +29,11 @@ addPlot(timeWindow=30, yLimits=[-2, 2])
 addSignal('MICROSTRAIN_INS',msg.utime, msg.gyro[0])
 addSignal('MICROSTRAIN_INS',msg.utime, msg.gyro[1])
 addSignal('MICROSTRAIN_INS',msg.utime, msg.gyro[2])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.gyro[0])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.gyro[1])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.gyro[2])
+
+
 
 def rot0Function(msg):
     '''rot0'''
@@ -41,8 +52,23 @@ addSignalFunction('ATLAS_IMU_BATCH', rot2Function)
 
 # ft plot
 addPlot(timeWindow=30, yLimits=[0, 1600])
+addSignal('FORCE_TORQUE',msg.utime, msg.sensors[0].force[0])
+addSignal('FORCE_TORQUE',msg.utime, msg.sensors[1].force[0])
+addSignal('FORCE_TORQUE',msg.utime, msg.sensors[0].force[1])
+addSignal('FORCE_TORQUE',msg.utime, msg.sensors[1].force[1])
 addSignal('FORCE_TORQUE',msg.utime, msg.sensors[0].force[2])
 addSignal('FORCE_TORQUE',msg.utime, msg.sensors[1].force[2])
 
 addSignal('ATLAS_STATE',msg.utime, msg.force_torque.l_foot_force_z)
 addSignal('ATLAS_STATE',msg.utime, msg.force_torque.r_foot_force_z)
+
+# orientation plot
+addPlot(timeWindow=30, yLimits=[1, 2])
+addSignal('MICROSTRAIN_INS',msg.utime, msg.quat[0])
+addSignal('MICROSTRAIN_INS',msg.utime, msg.quat[1])
+addSignal('MICROSTRAIN_INS',msg.utime, msg.quat[2])
+addSignal('MICROSTRAIN_INS',msg.utime, msg.quat[3])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.quat[0])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.quat[1])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.quat[2])
+addSignal('MICROSTRAIN_INS2',msg.utime, msg.quat[3])
